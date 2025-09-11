eee66d0355f0858e0ba63f34910fcd74e4bcc8de
PARDEEP 111 <pardeepsingh091209@gmail.com>
2025-09-11 19:10:07 +0530

    did some majour js and css changes and library changes

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
 console.log("Lets write some js!!")
   async function main(params) {
-  let a= await fetch("")  
+  let a= await fetch("http://127.0.0.1:5500/songs/")  
+  let response = await a.text();
+  // console.log(response);
+  let div = document.createElement("div")
+    div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  let songs=[]
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith("mp3")) {
+      songs.push(element.href.split("/songs/")[1]) 
+    }
+    
+  }
+return songs;
+    
  }
+ async function getSongs(params) {
+  let song = await main()
+  console.log(song);
+   let songul =document.querySelector(".your-library-song-container").getElementsByTagName("ul")[0]
+   for (const songs of song) {
+    songul.innerHTML = songul.innerHTML+`<li>
+
+                            <img src="assets/music_note_40dp_DCEAD5_FILL0_wght400_GRAD0_opsz40.png" alt="">
+                        <div class="info">
+                            <div class="song-name">${songs.replaceAll("%20", " ")}</div>
+                            <div class="artist">Pardeep</div>
+                        </div>
+                        <div class="play-now">
+                            <span> Play now</span>
+                            <img class="playlist-song-play-btn" src="assets/play_circle_40dp_D9D9D9_FILL0_wght400_GRAD0_opsz40.png" alt="">
+                        </div>
+                        
+     </li>`
+   }
+  let audio = new Audio(song[0])
+  audio.play();
+  audio.addEventListener("loadeddata", ()=>{
+    let duration = audio.duration;
+    console.log(audio.duration,audio.currentSrc,audio.currentTime)
+  })
+ }
+ getSongs()  
