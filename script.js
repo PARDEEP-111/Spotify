cdc282cd94e02c97a332831632212a264bc3380c
PARDEEP 111 <pardeepsingh091209@gmail.com>
2025-09-15 18:42:16 +0530

    started working on responsiveness

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
 console.log("Lets write some js!!")
 
-let currentSong= new Audio();
-function secondsToMinutesSconds(seconds){
-  if (isNaN(seconds)|| seconds < 0){
+let currentSong = new Audio();
+function secondsToMinutesSconds(seconds) {
+  if (isNaN(seconds) || seconds < 0) {
     return "invalid input";
   }
-  const minutes = Math.floor(seconds/60);
-  const remainingSeconds = Math.floor(seconds%60);
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
 
-  const formattedMinutes = String(minutes).padStart(2,'0')
-  const formattedSeconds = String(remainingSeconds).padStart(2,'0')
+  const formattedMinutes = String(minutes).padStart(2, '0')
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0')
 
-  return  `${formattedMinutes}:${formattedSeconds}`;
+  return `${formattedMinutes}:${formattedSeconds}`;
 }
-  async function main(params) {
-  let a= await fetch("http://127.0.0.1:5500/songs/")  
+async function main(params) {
+  let a = await fetch("http://127.0.0.1:5500/songs/")
   let response = await a.text();
   // console.log(response);
   let div = document.createElement("div")
-    div.innerHTML = response;
+  div.innerHTML = response;
   let as = div.getElementsByTagName("a");
-  let songs=[]
+  let songs = []
   for (let index = 0; index < as.length; index++) {
     const element = as[index];
     if (element.href.endsWith("mp3")) {
-      songs.push(element.href.split("/songs/")[1]) 
+      songs.push(element.href.split("/songs/")[1])
     }
-    
+
   }
-return songs;
-    
- }
- const PlayMusic= (track , pause= false)=>{
-  currentSong.src="/songs/" + track;
-  if(!pause){
+  return songs;
+
+}
+const PlayMusic = (track, pause = false) => {
+  currentSong.src = "/songs/" + track;
+  if (!pause) {
     currentSong.play()
-play.src="assets/pause_circle_40dp_EFEFEF_FILL0_wght400_GRAD0_opsz40.png"
+    play.src = "assets/pause_circle_40dp_EFEFEF_FILL0_wght400_GRAD0_opsz40.png"
   }
-// let audio  = new Audio()
-// play.src="assets/pause_circle_40dp_EFEFEF_FILL0_wght400_GRAD0_opsz40.png"
-document.querySelector(".song-info").innerHTML =decodeURI(track) 
-document.querySelector(".song-time").innerHTML = "00:00 / 00:00"
- }
- async function getSongs(params) {
-  
+  // let audio  = new Audio()
+  // play.src="assets/pause_circle_40dp_EFEFEF_FILL0_wght400_GRAD0_opsz40.png"
+  document.querySelector(".song-info").innerHTML = decodeURI(track)
+  document.querySelector(".song-time").innerHTML = "00:00 / 00:00"
+}
+async function getSongs(params) {
+
   let song = await main()
-  PlayMusic(song[0],true)
+  PlayMusic(song[0], true)
   console.log(song);
-   let songul =document.querySelector(".your-library-song-container").getElementsByTagName("ul")[0]
-   for (const songs of song) {
-    songul.innerHTML = songul.innerHTML+`<li>
+  let songul = document.querySelector(".your-library-song-container").getElementsByTagName("ul")[0]
+  for (const songs of song) {
+    songul.innerHTML = songul.innerHTML + `<li>
 
                             <img class="music-icon" src="assets/music_note_40dp_DCEAD5_FILL0_wght400_GRAD0_opsz40.png" alt="">
                         <div class="info">
@@ -61,37 +61,35 @@ document.querySelector(".song-time").innerHTML = "00:00 / 00:00"
                             <img class="playlist-song-play-btn" src="assets/play_circle_40dp_D9D9D9_FILL0_wght400_GRAD0_opsz40.png" alt="">
                         </div>              
      </li>`
-   }
-  Array.from(document.querySelector(".your-library-song-container").getElementsByTagName("li")).forEach(e=>{
-    e.addEventListener("click", element=>{
+  }
+  Array.from(document.querySelector(".your-library-song-container").getElementsByTagName("li")).forEach(e => {
+    e.addEventListener("click", element => {
       console.log(e.querySelector(".info").firstElementChild.innerHTML);
       PlayMusic(e.querySelector(".info").firstElementChild.innerHTML)
 
     })
-    play.addEventListener("click", ()=>{
-if(currentSong.paused){
-currentSong.play()
-play.src="assets/pause_circle_40dp_EFEFEF_FILL0_wght400_GRAD0_opsz40.png"
-}
-else{
-  currentSong.pause()
-  play.src="assets/play_circle_40dp_D9D9D9_FILL0_wght400_GRAD0_opsz40.png"
-}
+    play.addEventListener("click", () => {
+      if (currentSong.paused) {
+        currentSong.play()
+        play.src = "assets/pause_circle_40dp_EFEFEF_FILL0_wght400_GRAD0_opsz40.png"
+      }
+      else {
+        currentSong.pause()
+        play.src = "assets/play_circle_40dp_D9D9D9_FILL0_wght400_GRAD0_opsz40.png"
+      }
     })
 
   })
-  currentSong.addEventListener("timeupdate", ()=>{
-    console.log(currentSong.currentTime,currentSong.duration)
-    document.querySelector(".song-time").innerHTML = `${secondsToMinutesSconds(currentSong.currentTime)} / ${
-      secondsToMinutesSconds(currentSong.duration)
-    }`
-    document.querySelector(".circle").style.left=(currentSong.currentTime/currentSong.duration) *100 + "%"
+  currentSong.addEventListener("timeupdate", () => {
+    document.querySelector(".song-time").innerHTML = `${secondsToMinutesSconds(currentSong.currentTime)} / ${secondsToMinutesSconds(currentSong.duration)
+      }`
+    document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%"
   })
 
-  document.querySelector(".seek-bar").addEventListener("click", e=>{
-    let persent = (e.offsetX/e.target.getBoundingClientRect().width) *100
-    document.querySelector(".circle").style.left= (persent) * 100 +"%"
-    currentSong.currentTime = ((currentSong.duration)* persent ) / 100
+  document.querySelector(".seek-bar").addEventListener("click", e => {
+    let persent = (e.offsetX / e.target.getBoundingClientRect().width) * 100
+    document.querySelector(".circle").style.left = (persent) * 100 + "%"
+    currentSong.currentTime = ((currentSong.duration) * persent) / 100
   })
- }
- getSongs()  
+}
+getSongs()  
